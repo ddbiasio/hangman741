c4e69721adf170fa8dc222311a462ebc7b9987dd
ddbiasio <siobhanmdangelo@googlemail.com>
2024-03-12 18:12:37 +0000

    modified:   milestone_2.py code refactor

--- milestone_2.py
+++ milestone_2.py
@@ -1,19 +1,35 @@
 import random
 
-# List of 5 fruits
-word_list = ["banana", "apple", "watermelon", "grapes", "strawberries"]
-print(word_list)
+def select_word() -> str:
+    # List of 5 fruits
+    word_list = ["banana", "apple", "watermelon", "grapes", "strawberries"]
+    print(word_list)
 
-# Pick a word at random
-word = random.choice(word_list)
-print(word)
+    # Pick a word at random
+    word = random.choice(word_list)
+    print(word)
 
+    # retunr the selected word
+    return word
+
+def get_user_letter() -> str:
 # Get a letter from the user
-guess = input("Enter a letter you think is in the word: ")
-print(guess)
+    return input("Enter a letter you think is in the word: ")
+    print(guess)
 
-# Check the input is 1 letter
-if len(guess) == 1 and guess.isalpha():
-    print("Good Guess")
-else:
-    print("Oops! That is not a valid input")
+def is_input_valid(guess: str) -> bool:
+    # Check the input is 1 letter
+    if len(guess) == 1 and guess.isalpha():
+        print("Good Guess")
+        return True
+    else:
+        print("Oops! That is not a valid input")
+        return False
+    
+if __name__ == "__main__":
+    
+    word = select_word()
+    guess = get_user_letter()
+    if is_input_valid(guess):
+        # check the letter
+        pass
