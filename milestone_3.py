d6354237624a47df0516f4ff12e4486490438567
ddbiasio <siobhanmdangelo@googlemail.com>
2024-03-12 18:44:39 +0000

    modified:   milestone_3.py adding check_guess and ask_for_inout functions

--- milestone_3.py
+++ milestone_3.py
@@ -1,6 +1,14 @@
 import random
 
 def select_word() -> str:
+    """
+    select_word Selects a word froma list
+
+    Returns
+    -------
+    str
+        The selected word
+    """
     # List of 5 fruits
     word_list = ["banana", "apple", "watermelon", "grapes", "strawberries"]
     print(word_list)
@@ -12,32 +20,60 @@ def select_word() -> str:
     # retunr the selected word
     return word
 
-def get_user_letter() -> str:
-# Get a letter from the user
-    return input("Enter a letter you think is in the word: ")
-    print(guess)
+def ask_for_input() -> str:
+    """
+    ask_for_input Prompts user to enter a guess (single letter)
+    and validates this to be a single letter
 
-def is_input_valid(guess: str) -> bool:
-    # Check the input is 1 letter
-    if len(guess) == 1 and guess.isalpha():
-        print("Good Guess")
+    Returns
+    -------
+    str
+        The user's input
+    """
+    # Loop until a valid letter is entered
+    while True:
+
+        guess = input("Enter a letter you think is in the word: ")
+
+        # Check the input is 1 letter
+        
+        if len(guess) == 1 and guess.isalpha():
+            return guess
+        else:
+            print("Oops! That is not a valid input")
+
+    
+def check_guess(guess: str, word: str):
+    """
+    check_guess checks is a letter in the word
+
+    Parameters
+    ----------
+    guess : str
+        A single letter guess
+    word : str
+        The word being checked for 'guess'
+    Returns
+    -------
+    None
+       Nothing is returned
+    """
+    guess = guess.lower()
+
+    if guess in word:
+        print(f"Good guess! {guess} is in the word")
         return True
     else:
-        print("Oops! That is not a valid input")
+        print(f"Sorry, {guess} is not in the word. Try again.")
         return False
     
 if __name__ == "__main__":
 
     word = select_word()
 
-    while True:
+    guess = ask_for_input()
 
-        guess = get_user_letter()
-        if is_input_valid(guess):
-            # check the letter
-            break
+    check_guess(guess, word)
 
-    if guess in word:
-        print(f"Good guess! {guess} is in the word")
-    else:
-        print(f"Sorry, {guess} is not in the word. Try again.")
+
+
