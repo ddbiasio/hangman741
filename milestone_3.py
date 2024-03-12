48c38261b9f5e658cf7dc5e41e2e76d68b8ada62
ddbiasio <siobhanmdangelo@googlemail.com>
2024-03-12 18:27:43 +0000

    new file:   milestone_3.py Added functions and iteratively get user inout until a valid value entered

---
+++ milestone_3.py
@@ -0,0 +1,37 @@
+import random
+
+def select_word() -> str:
+    # List of 5 fruits
+    word_list = ["banana", "apple", "watermelon", "grapes", "strawberries"]
+    print(word_list)
+
+    # Pick a word at random
+    word = random.choice(word_list)
+    print(word)
+
+    # retunr the selected word
+    return word
+
+def get_user_letter() -> str:
+# Get a letter from the user
+    return input("Enter a letter you think is in the word: ")
+    print(guess)
+
+def is_input_valid(guess: str) -> bool:
+    # Check the input is 1 letter
+    if len(guess) == 1 and guess.isalpha():
+        print("Good Guess")
+        return True
+    else:
+        print("Oops! That is not a valid input")
+        return False
+    
+if __name__ == "__main__":
+
+    while True:
+
+        guess = get_user_letter()
+        if is_input_valid(guess):
+            # check the letter
+            break
+
